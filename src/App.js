d9b146d65baf9e76bc5207259c00ca87697fffcd
Edmond <edmond.santiago7@gmail.com>
2024-01-10 19:53:53 +1100

    import other components to App component, import BrowserRouter and Route for application's routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Friends from "./components/Friends";
+import FriendDetails from "./components/FriendDetails";
+import User from "./components/User";
 
 function App() {
+
   return (
     <>
-      <Login setIsLoggedIn={setIsLoggedIn} />
+      <Switch>
+        <Route path="/login">
+          <Login setIsLoggedIn={setIsLoggedIn} />
+        </Route>
+
+        <Route exact path="/">
+          <Header onCreatePostClick={handleCreatePostClick} />
+          <Home isLoggedIn={isLoggedIn} createPost={createPost} />
+        </Route >
+
+        <Route exact path="/friends/:id">
+          <Header />
+          <FriendDetails />
+        </Route>
+
+        <Route path="/friends">
+          <Header />
+          <Friends />
+        </Route>
+
+        <Route path="/user">
+          <Header />
+          <User />
+        </Route>
+      </Switch>
     </>
   );
 }
